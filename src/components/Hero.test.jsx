2408232154303d36f07e6c114643bbc0d8a30bcb
Sean Barry <seanandrewbarry@protonmail.com>
2025-07-07 17:36:54 -0600

    Added a simple test

---
+++ src/components/Hero.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Hero from './Hero';
+
+describe('Hero component', () => {
+  it('renders the hero heading', () => {
+    render(
+      <MemoryRouter>
+        <Hero />
+      </MemoryRouter>
+    );
+    expect(screen.getByRole('heading', { name: /Sean Andrew Barry/i })).toBeInTheDocument();
+  });
+});
