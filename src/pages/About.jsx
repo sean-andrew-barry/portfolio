915778e87f1cb6e4c90b4c399843c1bef16d0ecf
Sean Barry <seanandrewbarry@protonmail.com>
2025-07-21 14:49:06 -0600

    Fixed dead link

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -158,7 +158,7 @@ export default function About() {
               Contact me
             </Link>
             <Link
-              to="/contact"
+              to="/projects"
               className="border-2 border-white text-white px-8 py-4 rounded-lg font-semibold hover:bg-white hover:text-purple-600 transition-colors"
             >
               View My Work
